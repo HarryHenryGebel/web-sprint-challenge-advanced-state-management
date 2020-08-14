ddfffe61c61c3bb476193952c0a0f4640096f89f
Harry Henry Gebel <hhgebel@gmail.com>
2020-08-14 13:59:40 -0400

    Complete GET_SMURFS

--- smurfs/src/actions.js
+++ smurfs/src/actions.js
@@ -1,6 +1,8 @@
 import requester from "easier-requests";
 
 export const GET_SMURFS = "GET_SMURFS";
+export const GET_SMURFS_SUCCESS = "GET_SMURFS_SUCCESS";
+export const GET_SMURFS_FAILURE = "GET_SMURFS_FAILURE";
 
 export function getSmurfs() {
   return function (dispatch) {
@@ -8,11 +10,12 @@ export function getSmurfs() {
       try {
         const id = requester.createUniqueID();
         await requester.get("http://localhost:3333/smurfs", id);
-        console.log(requester.response(id));
+        dispatch({type: GET_SMURFS_SUCCESS,
+                  smurfs: requester.response(id).data});
       }
       catch (error) {
         console.log(error);
-        throw error;
+        dispatch({type: GET_SMURFS_FAILURE, error: error});
       }
     }
     dispatch({type: GET_SMURFS});
--- smurfs/src/reducer.js
+++ smurfs/src/reducer.js
@@ -1,9 +1,11 @@
-import GET_SMURFS from "./actions";
+import {GET_SMURFS, GET_SMURFS_FAILURE, GET_SMURFS_SUCCESS} from "./actions";
+
 
 export const initialState = {
   smurfs: [],
   isFetching: false,
-  error: ""
+  error: "",
+  initialized: false
 };
 
 export default function reducer(state = initialState, action) {
@@ -12,6 +14,14 @@ export default function reducer(state = initialState, action) {
     return {...state,
             isFetching: true,
             error: null};
+  case GET_SMURFS_FAILURE:
+    return {...state,
+            isFetching: false,
+            error: action.error};
+  case GET_SMURFS_SUCCESS:
+    return {...state,
+            isFetching: false,
+            smurfs: action.smurfs};
   default:
     // return state if it is called by redux, otherwise throw error
     if (action.type.includes("@@redux"))
