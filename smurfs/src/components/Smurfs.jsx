59c2b526bd537a00b0fd1d1c9d081b3a7b30b22d
Harry Henry Gebel <hhgebel@gmail.com>
2020-08-14 15:13:54 -0400

    Connect to store

--- smurfs/src/components/Smurfs.jsx
+++ smurfs/src/components/Smurfs.jsx
@@ -1,8 +1,42 @@
 import React from "react";
+import {connect} from "react-redux";
 
-export default function Smurfs () {
-  return (
-    <>
-    </>
-  );
+import {getSmurfs} from "../actions";
+
+function Smurfs (props) {
+  const {error, getSmurfs, initialized, isFetching, smurfs} = props;
+  debugger;
+
+  if (!initialized) {
+    getSmurfs();
+  }
+
+  if (initialized && !isFetching && !error)
+    return (
+      <>
+        {smurfs.map(smurf => <div/>)}
+      </>
+    );
+  else if (!error)
+    return (
+      <h2>
+        Loading...
+      </h2>);
+  else
+    return (
+      <h2>
+        Error: {`${error}`}
+      </h2>
+    );
 }
+
+function mapStateToProps (state) {
+  return {
+  error: state.error,
+  initialized: state.initialized,
+  isFetching: state.isFetching,
+  smurfs: state.smurfs
+  };
+}
+
+export default connect(mapStateToProps, {getSmurfs})(Smurfs);
